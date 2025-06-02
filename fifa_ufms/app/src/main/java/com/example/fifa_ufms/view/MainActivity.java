1672c6aed9d5f755325665d46b184f470b48685f
CaioTome <felipe3005cf@gmail.com>
2025-06-02 15:26:22 -0400

    Banco de Dados 100% funcional ctz. Além disso, agora tem um Data Seeder com 4 jogadores, 2 times e uma partida

--- fifa_ufms/app/src/main/java/com/example/fifa_ufms/view/MainActivity.java
+++ fifa_ufms/app/src/main/java/com/example/fifa_ufms/view/MainActivity.java
@@ -7,6 +7,12 @@ import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fifa_ufms.R;
+import com.example.fifa_ufms.database.CampeonatoDatabase;
+import com.example.fifa_ufms.entities.Jogador;
+import com.example.fifa_ufms.entities.Partida;
+import com.example.fifa_ufms.entities.Time;
+
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +20,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inserir time padrão
+        Executors.newSingleThreadExecutor().execute(() -> {
+            CampeonatoDatabase db = CampeonatoDatabase.getInstance(getApplicationContext());
+
+            if (db.timeDao().listarTodosTimes().isEmpty()) {
+                Time time = new Time("UFMS", "Branco e Azul");
+                db.timeDao().inserirTime(time);
+
+                Time time2 = new Time("UEMS", "Preto e Vermelho");
+                db.timeDao().inserirTime(time2);
+
+                // Inserir jogadores
+                Jogador jogador1 = new Jogador(
+                        0, 1, 5, "2001-04-15", "caio@ufms.br", "Caio", "Caio", 1
+                );
+                Jogador jogador2 = new Jogador(
+                        1, 0, 3, "2000-10-30", "txas@ufms.br", "Txas", "Texeira", 1
+                );
+                Jogador jogador3 = new Jogador(
+                        0, 2, 4, "2002-02-28", "Sora@uems.br", "Sora", "Soraya", 2
+                );
+                Jogador jogador4 = new Jogador(
+                        1, 1, 2, "1999-11-11", "Sophy@uems.br", "Sophy", "Sophya", 2
+                );
+
+                db.jogadorDao().inserirJogador(jogador1);
+                db.jogadorDao().inserirJogador(jogador2);
+                db.jogadorDao().inserirJogador(jogador3);
+                db.jogadorDao().inserirJogador(jogador4);
+
+                // Inserir partida
+                Partida partida = new Partida("2025-06-01", 1, 2, 3, 2);
+                db.partidaDao().inserirPartida(partida);
+            }
+        });
 
         // Card para Times
         LinearLayout cardTimes = findViewById(R.id.card_times);
